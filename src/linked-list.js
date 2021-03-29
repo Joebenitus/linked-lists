5e002ec07c691b59eccc99ea3e6f59fccdb7ae13
Joseph Karnafel <josephkarnafel@gmail.com>
2021-03-29 14:36:42 -0700

    initial commit. implement basic linked list class with add and remove node methods

---
+++ src/linked-list.js
@@ -0,0 +1,40 @@
+import Node from './node.js';
+
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertLast(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next != null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+  }
+
+  remove(indexToRemove) {
+    if (indexToRemove === 0) {
+      this.head = this.head.next;
+    } else {
+      let currentNode = this.head;
+      let currentIndex = 0;
+      while ((currentIndex + 1) <= indexToRemove) {
+        if (currentNode.next === null) {
+          return -1;
+        }
+        if ((currentIndex + 1) === indexToRemove) {
+          currentNode.next = currentNode.next.next;
+          break;
+        }
+        currentNode = currentNode.next;
+        currentIndex++;
+      }
+    }
+  }
+}
